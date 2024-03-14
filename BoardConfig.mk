55b541a829980d4b57ff244f50873e5f47355432
aayowtf <atharv2951@proton.me>
2024-03-14 12:53:42 +0530

    ceres: Create empty manifest file
    Signed-off-by: aayowtf <atharv2951@proton.me>

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -14,5 +14,8 @@ BOARD_BOOTIMAGE_PARTITION_SIZE := 33554432
 
 BOARD_SAMSUNG_DYNAMIC_PARTITIONS_SIZE := 7843348480
 
+# Vintf
+DEVICE_MANIFEST_FILE += $(DEVICE_PATH)/configs/vintf/manifest.xml
+
 # Inherit from mt6765-common
 include device/samsung/mt6765-common/BoardConfigCommon.mk
