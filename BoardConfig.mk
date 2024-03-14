496f50fd7121258cfc5eb30248f1f39ea112754d
aayowtf <atharv2951@proton.me>
2024-03-14 12:48:26 +0530

    ceres: Configure Partitions
    Signed-off-by: aayowtf <atharv2951@proton.me>

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -6,5 +6,10 @@
 
 DEVICE_PATH := device/samsung/ceres
 
+# Partitions
+BOARD_BOOTIMAGE_PARTITION_SIZE := 33554432
+
+BOARD_SAMSUNG_DYNAMIC_PARTITIONS_SIZE := 7843348480
+
 # Inherit from mt6765-common
 include device/samsung/mt6765-common/BoardConfigCommon.mk
